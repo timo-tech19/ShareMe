80a7cd262269e9a85962638ad3fd48632ce60619
Timo Heman <timoheman16@gmail.com>
2022-02-06 01:30:02 +0100

    add schemas for sanity

---
+++ backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: "user",
+  title: "User",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "string",
+    },
+  ],
+};
