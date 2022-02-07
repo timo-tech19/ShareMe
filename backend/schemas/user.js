7d0c791b26117a2820283303c9c8d15431235aad
Timo Heman <timoheman16@gmail.com>
2022-02-08 00:59:04 +0100

    add google authentication

--- backend/schemas/user.js
+++ backend/schemas/user.js
@@ -9,6 +9,11 @@ export default {
       type: "string",
     },
     {
+      name: "email",
+      title: "Email",
+      type: "string",
+    },
+    {
       name: "image",
       title: "Image",
       type: "string",
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,70 @@
-import React from 'react';
+import React from "react";
+import GoogleLogin from "react-google-login";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import ShareMeVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+import { client } from "../containers/client";
 
 function Login() {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const responseGoogle = async (response) => {
+    localStorage.setItem("user", JSON.stringify(response.profileObj));
+
+    const { googleId, name, email, imageUrl } = response.profileObj;
+
+    const sanityUser = {
+      _id: googleId,
+      _type: "user",
+      name,
+      email,
+      image: imageUrl,
+    };
+
+    await client.createIfNotExists(sanityUser);
+    navigate("/", { replace: true });
+  };
+  return (
+    <div className='flex justify-center items-center flex-col h-screen'>
+      <div className='relative h-full w-full'>
+        <video
+          src={ShareMeVideo}
+          type='video/mp4'
+          autoPlay
+          loop
+          controls={false}
+          muted
+          className='h-full w-full object-cover'
+        />
+        <div className='absolute flex flex-col justify-center items-center top-0 left-0 right-0 bottom-0 bg-blackOverlay'>
+          <div className='p-5'>
+            <img src={logo} width='130px' alt='Share me logo' />
+          </div>
+
+          <div className='shadow-2xl'>
+            <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+              render={({ onClick, disabled }) => (
+                <button
+                  onClick={onClick}
+                  disabled={disabled}
+                  className='bg-mainColor p-3 rounded-lg flex items-center outline-none'
+                >
+                  <FcGoogle />
+                  <span className='ml-2 capitalize'>Sign In With Google</span>
+                </button>
+              )}
+              // onSuccess={responseGoogle}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy='single_host_origin'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
