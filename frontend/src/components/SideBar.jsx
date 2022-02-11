38744a49278ccc93d1b867aed7d390a336414655
Timo Heman <timoheman16@gmail.com>
2022-02-11 21:38:42 +0100

    create side bar

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,7 +1,83 @@
 import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { RiHomeFill } from "react-icons/ri";
+import { IoIosArrowForward } from "react-icons/io";
 
-const SideBar = () => {
-  return <div>SideBar</div>;
+import logo from "../assets/logo.png";
+
+const notActiveStyles =
+  "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
+const activeStyles =
+  "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
+
+const categories = [
+  { name: "Animals" },
+  { name: "Wallpapers" },
+  { name: "Gaming" },
+  { name: "Photography" },
+  { name: "Coding" },
+];
+
+const SideBar = ({ user, closeToggle }) => {
+  const handleCloseSideBar = () => {
+    if (closeToggle) closeToggle(false);
+  };
+
+  return (
+    <div className='flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar'>
+      <div className='flex flex-col'>
+        <Link
+          to='/'
+          onClick={handleCloseSideBar}
+          className='flex px-5 gap-2 my-6 pt-1 w-190 items-center'
+        >
+          <img src={logo} alt='Share Me Logo' className='w-full' />
+        </Link>
+        <div className='flex flex-col gap-5 w-full'>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              isActive ? activeStyles : notActiveStyles
+            }
+            onClick={handleCloseSideBar}
+          >
+            <RiHomeFill />
+            Home
+          </NavLink>
+          <h3 className='mt-2 px-5 text-base 2xl:text-xl'>
+            Discover Categories
+          </h3>
+          {categories.slice(0, categories.length - 1).map(({ name }) => (
+            <NavLink
+              key={name}
+              to={`/category/${name}`}
+              className={({ isActive }) =>
+                isActive ? activeStyles : notActiveStyles
+              }
+              onClick={handleCloseSideBar}
+            >
+              {name}
+            </NavLink>
+          ))}
+        </div>
+      </div>
+
+      {user && (
+        <Link
+          to={`/profile/${user._id}`}
+          className='flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg'
+          onClick={handleCloseSideBar}
+        >
+          <img
+            src={user.image}
+            className='w-10 h-10 rounded-full'
+            alt='User profile'
+          />
+          <p>{user.name}</p>
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default SideBar;
--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -30,9 +30,9 @@ function Home() {
   return (
     <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
       <div className='hidden md:flex h-screen flex-initial'>
-        <SideBar user={user && user} closeToggle={setToggleSideBar} />
+        <SideBar user={user && user} />
       </div>
-      <div className='flex md:hidden flex-row'>
+      <div className='flex relative md:hidden flex-row'>
         <div className='p-2 w-full flex flex-row justify-between items-center shadow-md'>
           <HiMenu
             fontSize={40}
@@ -48,8 +48,8 @@ function Home() {
         </div>
         {toggleSideBar && (
           <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-sm z-10 animate-slide-in'>
-            <div className='absolute w-full flex justify-between items-center p-2'>
-              <SideBar user={user && user} closeToggle={setToggleSideBar} />
+            <SideBar user={user && user} closeToggle={setToggleSideBar} />
+            <div className='absolute top-4 right-4 flex justify-between items-center p-2'>
               <AiFillCloseCircle
                 fontSize={30}
                 className='cursor-pointer'
